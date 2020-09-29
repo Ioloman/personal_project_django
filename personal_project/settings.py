358548357102e474be48fe1e5b5e87bed37b2353
kirilldd2 <kirilldd2@gmail.com>
2020-09-29 11:36:10 +0000

    production changes

--- personal_project/settings.py
+++ personal_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'ydpjrg#hyoh0s2zryo6l$8noitw&#chh6dfmgfzwgt*%-(7k0*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kirilld.pythonanywhere.com']
 
 
 # Application definition
@@ -121,8 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_URL = '/media/'
